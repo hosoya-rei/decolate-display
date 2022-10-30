96824b2ecf152b663baca1c866fa419ddbf5446d
Rei Hosoya <reirei.hoso.boz@gmail.com>
2022-10-30 14:15:05 +0000

    ジャンケンゲームの実装

--- hello.c
+++ hello.c
@@ -1,13 +1,27 @@
 #include "logic.h"
 
 int main(void) {
-  for (int i = 10; i > 0; i--) {
-    printf("\r%d ", i);
-    fflush(stdout);
-    sleep(1000);
+  int com, you;
+  printf("ジャンケンゲーム");
+  srand((unsigned int)time(NULL));
+  while (1) {
+    com = rand() %3 +1;
+    printf("あなたの手を入力(1=グー, 2=パー, 3=チョキ, 0=終了):"); scanf("%d", &you);
+    if (you == 0) {
+	break;
+    }else if (you != 1 && you != 2 && you != 3) {
+	continue;
+    }else {
+	printf("コンピュータの手:%d\n", com);
+	if ((you == 1 && com == 3) || (you == 2 && com == 1) || (you == 3 && com == 2)) { //youの勝ちパターン
+		printf("あなたの勝ち\n");
+	}else if ((com == 1 && you == 3) || (com == 2 && you == 1) || (com == 3 && you == 2)) { //comの勝ちパターン
+		printf("コンピュータの勝ち\n");
+	}else {
+		printf("あいこ\n");
+		continue;
+	}
+    }
   }
-  printf("\rFIRE!\n");
-  printf("経過時間:%lf秒\n", (double)clock() / CLOCKS_PER_SEC);
- 
- return 0; 
+return 0; 
 }
--- logic.h
+++ logic.h
@@ -4,6 +4,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <string.h>
+
 
 int sleep(unsigned long x);
 
